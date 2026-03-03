eeac7f5403c9bb17032380eef8cdc96ab9dca982
priyanshumore99 <priyanshumore99@gmail.com>
2026-03-03 09:53:32 +0530

    Change data types in crm_sales_details table

--- scripts/Silver/ddl_silver.sql
+++ scripts/Silver/ddl_silver.sql
@@ -39,20 +39,20 @@ CREATE TABLE silver.crm_prd_info (
 );
 GO
 
-IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
+	IF OBJECT_ID ('silver.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details;
-CREATE TABLE silver.crm_sales_details ( 
-	sls_ord_num NVARCHAR(50), 
-	sls_prd_key NVARCHAR(50), 
-	sls_cust_id INT, 
-	sls_order_dt INT,
-	sls_ship_dt INT, 
-	sls_due_dt INT, 
-	sls_sales INT,
-	sls_quantity INT,
-	sls_price INT,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
-);
+	CREATE TABLE silver.crm_sales_details ( 
+		sls_ord_num NVARCHAR(50), 
+		sls_prd_key NVARCHAR(50), 
+		sls_cust_id INT, 
+		sls_order_dt DATE,
+		sls_ship_dt DATE, 
+		sls_due_dt DATE, 
+		sls_sales INT,
+		sls_quantity INT,
+		sls_price INT,
+		dwh_create_date DATETIME2 DEFAULT GETDATE()
+	);
 GO
 
 IF OBJECT_ID ('silver.erp_loc_a101', 'U') IS NOT NULL
